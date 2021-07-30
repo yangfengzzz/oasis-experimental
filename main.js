2126111004b889c244712c666901005c5ce5617c
yangfengzzz <yangfengzzz@hotmail.com>
2021-07-30 11:52:33 +0800

    redesing material

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-// import * as simulator from "./src/Simulator";
-// import {onLoad} from "./src/physx.release";
-//
-// const update = () => {
-//     simulator.update()
-//     requestAnimationFrame(update)
-// }
-//
-// onLoad(() => {
-//     simulator.init()
-//
-//     update()
-// })
+import * as simulator from "./src/Simulator";
+import {onLoad} from "./src/physx.release";
 
-import * as simulator from "./src/RenderToTexture";
+const update = () => {
+    simulator.update()
+    requestAnimationFrame(update)
+}
+
+onLoad(() => {
+    simulator.init()
+
+    update()
+})
+
+// import * as simulator from "./src/RenderToTexture";
