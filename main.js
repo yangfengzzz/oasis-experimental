85c11e16b892914819af5559a45b2112edbd9cf6
yangfengzzz <yangfengzzz@hotmail.com>
2021-08-11 16:26:14 +0800

    add pvd in main

--- main.js
+++ main.js
@@ -5,6 +5,44 @@ import {PHYSX} from "./src/physx.release";
 export let physics
 export let PhysX
 
+const host = '127.0.0.1';
+const port = 8090;
+let socket: WebSocket;
+let queue: any[] = [];
+const pvdTransportImpl = PhysX.IPvdTransport.implementation({
+    connect: function () {
+        socket = new WebSocket(`wsl://$(host):$(port)`, ['binary'])
+        socket.onopen = () => {
+            console.log('Connected to PhysX Debugger');
+            queue.forEach(data => socket.send(data));
+            queue = []
+        }
+        socket.onclose = () => {
+        }
+        return true
+    },
+    disconnect: function () {
+        console.log("Socket disconnect")
+    },
+    isConnected: function () {
+    },
+    write: function (inBytes: number, inLength: number) {
+        const data = PhysX.HEAPU8.slice(inBytes, inBytes + inLength)
+        if (socket.readyState === WebSocket.OPEN) {
+            if (queue.length) {
+                queue.forEach(data => socket.send(data));
+                queue.length = 0;
+            }
+            socket.send(data);
+        } else {
+            queue.push(data);
+        }
+        return true;
+    }
+})
+
+const pvdTransport = new PhysX.ccPvdTransport(pvdTransportImpl);
+
 const setup = () => {
     const version = PhysX.PX_PHYSICS_VERSION
     const defaultErrorCallback = new PhysX.PxDefaultErrorCallback()
@@ -16,15 +54,14 @@ const setup = () => {
     )
 
     const gPvd = PhysX.PxCreatePvd(foundation);
-    const transport = PhysX.PxDefaultPvdSocketTransportCreate();
-    // gPvd.connect(transport, new PhysX.PxPvdInstrumentationFlags(PhysX.PxPvdInstrumentationFlag.eALL.value));
+    gPvd.connect(pvdTransport, new PhysX.PxPvdInstrumentationFlags(PhysX.PxPvdInstrumentationFlag.eALL.value));
 
     physics = PhysX.PxCreatePhysics(
         version,
         foundation,
         new PhysX.PxTolerancesScale(),
-        false,
-        null
+        true,
+        gPvd
     )
     PhysX.PxInitExtensions(physics, null)
 }
