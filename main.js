ea70cb69f60382049a692da116fda464a05d3b5f
yangfengzzz <yangfengzzz@hotmail.com>
2021-08-18 09:46:11 +0800

    update release wasm

--- main.js
+++ main.js
@@ -9,37 +9,37 @@ let socket;
 let queue = [];
 
 const setup = () => {
-    const pvdTransport = PhysX.PxPvdTransport.implement({
-        connect: function () {
-            socket = new WebSocket('ws://127.0.0.1:8090', ['binary'])
-            socket.onopen = () => {
-                console.log('Connected to PhysX Debugger');
-                queue.forEach(data => socket.send(data));
-                queue = []
-            }
-            socket.onclose = () => {
-            }
-            return true
-        },
-        disconnect: function () {
-            console.log("Socket disconnect")
-        },
-        isConnected: function () {
-        },
-        write: function (inBytes, inLength) {
-            const data = PhysX.HEAPU8.slice(inBytes, inBytes + inLength)
-            if (socket.readyState === WebSocket.OPEN) {
-                if (queue.length) {
-                    queue.forEach(data => socket.send(data));
-                    queue.length = 0;
-                }
-                socket.send(data);
-            } else {
-                queue.push(data);
-            }
-            return true;
-        }
-    })
+    // const pvdTransport = PhysX.PxPvdTransport.implement({
+    //     connect: function () {
+    //         socket = new WebSocket('ws://127.0.0.1:8090', ['binary'])
+    //         socket.onopen = () => {
+    //             console.log('Connected to PhysX Debugger');
+    //             queue.forEach(data => socket.send(data));
+    //             queue = []
+    //         }
+    //         socket.onclose = () => {
+    //         }
+    //         return true
+    //     },
+    //     disconnect: function () {
+    //         console.log("Socket disconnect")
+    //     },
+    //     isConnected: function () {
+    //     },
+    //     write: function (inBytes, inLength) {
+    //         const data = PhysX.HEAPU8.slice(inBytes, inBytes + inLength)
+    //         if (socket.readyState === WebSocket.OPEN) {
+    //             if (queue.length) {
+    //                 queue.forEach(data => socket.send(data));
+    //                 queue.length = 0;
+    //             }
+    //             socket.send(data);
+    //         } else {
+    //             queue.push(data);
+    //         }
+    //         return true;
+    //     }
+    // })
 
     //------------------------------------------------------------------------------------------------------------------
     const version = PhysX.PX_PHYSICS_VERSION
@@ -51,15 +51,15 @@ const setup = () => {
         defaultErrorCallback
     )
 
-    const gPvd = PhysX.PxCreatePvd(foundation);
-    gPvd.connect(pvdTransport, new PhysX.PxPvdInstrumentationFlags(PhysX.PxPvdInstrumentationFlag.eALL.value));
+    // const gPvd = PhysX.PxCreatePvd(foundation);
+    // gPvd.connect(pvdTransport, new PhysX.PxPvdInstrumentationFlags(PhysX.PxPvdInstrumentationFlag.eALL.value));
 
     physics = PhysX.PxCreatePhysics(
         version,
         foundation,
         new PhysX.PxTolerancesScale(),
-        true,
-        gPvd
+        false,
+        null
     )
     PhysX.PxInitExtensions(physics, null)
 }
